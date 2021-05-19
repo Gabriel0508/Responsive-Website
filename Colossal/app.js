33352ce8ed06f4efc983c687ead04825778e30a1
Gabriel Grad <65502252+Gabriel0508@users.noreply.github.com>
2021-05-19 14:52:10 +0300

    change style

--- Colossal/app.js
+++ Colossal/app.js
@@ -1,37 +1,34 @@
+
+
 //Navbar scroll change
+window.addEventListener("scroll", scrollNav);
 
-      window.addEventListener('scroll', scrollNav);
-
-      function scrollNav() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("navbar-bottom").style.top = "0";
-          } else {
-            document.getElementById("navbar-bottom").style.top = "-300px";
-            
-          }
-      }
-
+function scrollNav() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar-bottom").style.top = "0";
+  } else {
+    document.getElementById("navbar-bottom").style.top = "-300px";
+  }
+}
 
 //Burger animation
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
     menuOpen = true;
   } else {
-    menuBtn.classList.remove('open');
+    menuBtn.classList.remove("open");
     menuOpen = false;
   }
 });
 
 //Burger Content
+const btn = document.querySelector("#btn");
+const myDropdown = document.querySelector("#myDropdown");
 
-const btn = document.querySelector('#btn');
-const myDropdown = document.querySelector('#myDropdown');
-
-btn.addEventListener('click', () => {
-  myDropdown.classList.toggle("show")
-})
-  
+btn.addEventListener("click", () => {
+  myDropdown.classList.toggle("show");
+});
